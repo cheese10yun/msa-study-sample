c03eecc5d8eabefc4b2a8f085789f42bd5317366
Yun <cheese10yun@gmail.com>
2019-05-24 02:37:38 +0900

    dev 추가

--- microservice-a/src/main/java/com/cloud/microservicea/ApplicationConfiguration.java
+++ microservice-a/src/main/java/com/cloud/microservicea/ApplicationConfiguration.java
@@ -2,15 +2,17 @@ package com.cloud.microservicea;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties("application")
+//@ConfigurationProperties("application")
 @Getter
-@Setter
+//@Setter
 public class ApplicationConfiguration {
 
+  @Value("${application.message}")
   private String message;
 
 }
