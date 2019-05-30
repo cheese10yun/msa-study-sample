69ac8eac60150f8131cca64664dd0e88d31e960f
Yun <cheese10yun@gmail.com>
2019-05-30 23:57:47 +0900

    add 유레카

--- microservice-a/src/main/java/com/cloud/microservicea/MicroserviceAApplication.java
+++ microservice-a/src/main/java/com/cloud/microservicea/MicroserviceAApplication.java
@@ -2,8 +2,10 @@ package com.cloud.microservicea;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MicroserviceAApplication {
 
   public static void main(String[] args) {
--- service-consumer/src/main/java/com/cloud/serviceconsumer/ServiceConsumerApplication.java
+++ service-consumer/src/main/java/com/cloud/serviceconsumer/ServiceConsumerApplication.java
@@ -2,10 +2,12 @@ package com.cloud.serviceconsumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.cloud.serviceconsumer")
+@EnableDiscoveryClient
 public class ServiceConsumerApplication {
 
   public static void main(String[] args) {
