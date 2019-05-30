df0381892b169a6d6b6fb57d8acdef990bd72552
Yun <cheese10yun@gmail.com>
2019-05-31 01:21:51 +0900

    Add api gateway 추가

--- service-consumer/src/main/java/com/cloud/serviceconsumer/RandomServiceProxy.java
+++ service-consumer/src/main/java/com/cloud/serviceconsumer/RandomServiceProxy.java
@@ -5,11 +5,11 @@ import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "microservice-a")
+@FeignClient(name = "zuul-api-gateway")
 @RibbonClient(name = "microservice-a")
 public interface RandomServiceProxy {
 
-  @GetMapping("/random")
+  @GetMapping("/microservice-a/random")
   List<Integer> getRandomNumbers();
 
 }
